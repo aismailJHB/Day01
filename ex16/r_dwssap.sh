6ac45bf821644cb1636b0cd353cfb847c86e17e5
Aadil Ismail <aismail@c5r10s6.wethinkcode.co.za>
2020-06-19 09:51:45 +0200

    r_dwssap.sh

---
+++ ex16/r_dwssap.sh
@@ -0,0 +1,1 @@
+cat /etc/passwd | grep -v '#' | sed -n "p;n" | cut -d : -f 1 | awk 'NR%2==0' | rev | sort -r | awk "NR >= ($FT_LINE1-1) && NR <= ($FT_LINE2-1)" | sed -e ':a' -e 'N' -e '$!ba' -e 's/\n/, /g' -e '$s/$/./g' | tr "\n"
